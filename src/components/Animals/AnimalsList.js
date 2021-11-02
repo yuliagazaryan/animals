73a1b3f2e98e58ebec1916a5c6dd124cc1f89a7b
BC-macair010 <bc-macair010@BC-macair010s-MacBook-Air.local>
2021-11-02 14:36:24 +0200

    added input

--- src/components/Animals/AnimalsList.js
+++ src/components/Animals/AnimalsList.js
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import { animals } from "./animals";
 import AnimalsCard from "./AnimalsCard";
 
 import { Switch, Route, useRouteMatch } from "react-router-dom";
 import AnimalSingle from "./AnimalSingle";
 
-const AnimalsList = () => {
-  const match = useRouteMatch();
+class AnimalsList extends Component {
+  state ={
+    searchInput: "",
+  }
 
-  console.log(match);
-
-  const animalslisting = animals.map((item) => (
+  searchInputHandler = (event) => {
+    this.setState({
+      searchInput: event.target.value,
+    });
+    console.log(this.setState.searchInput);
+  }
+   animalslisting = animals.map((item) => (
     <AnimalsCard key={item.name} name={item.name} />
   ));
+ 
+    render() {
+
+      const animalFilter = animals.filter(animal => {
+        return animal.name
+        .toLowerCase()
+        .includes(this.state.searchInput.toLowerCase());
+      });
+
+      const animalslisting = animalFilter.map((item) => (
+        <AnimalsCard key={item.name} name={item.name} />
+      ));
 
   return (
     <div className="animalsList">
       <Switch>
-        <Route exact path={match.path}>
+        <Route exact path={this.props.match.path}>
+          <input type="text" onChange={this.searchInputHandler}/>
           {animalslisting}
         </Route>
-        <Route path={`${match.path}/:animal`}>
+        <Route path={`${this.props.match.path}/:animal`}>
           <AnimalSingle />
         </Route>
       </Switch>
     </div>
   );
 };
-
+}
 export default AnimalsList;
